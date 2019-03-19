cbbddbf665161496771cd9d8b29694759ca41bd8
Wojciech Jaromin <wojciech.jaromin@gmail.com>
2019-03-19 08:54:43 +0100

    Initial Commit. Added gitignore, license and readme files. Added basic projects and a solution file.

---
+++ Tests/SingletonTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Tests
+{
+    public class SingletonTests
+    {
+        [Fact]
+        public void SingletonTest()
+        {
+            throw new NotImplementedException("This test has not been implemented yet.");
+        }
+    }
+}
