1e78a36772d4f2b1c6358b186317bd337f829d75
Wojciech Jaromin <wojciech.jaromin@gmail.com>
2019-05-07 22:06:21 +0200

    Added a basic example of the Decorator Pattern.

---
+++ Tests/Decorator_Tests.cs
@@ -0,0 +1,22 @@
+﻿using Decorator;
+using Xunit;
+
+namespace Tests
+{
+    public class Decorator_Tests
+    {
+        [Fact]
+        public void CarPriceUpdated_WhenISelectEngineAndGearbox()
+        {
+            // Given
+            Car car = new Limousine();
+
+            // When
+            car = new DieselEngine(car);
+            car = new AutomaticGearbox(car);
+
+            // Then
+            Assert.Equal(114201, car.GetCost());
+        }
+    }
+}
